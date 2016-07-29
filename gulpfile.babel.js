410c0c40fc4558d5fb261670ddc05faac7eb6fcb
Catalin <catalin4ic@gmail.com>
2016-07-29 17:26:59 +0300

    scss build

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -13,29 +13,25 @@ const devDir = 'dev';
 const appDir = 'app';
 
 
-gulp.task('build:html',()=>{
+gulp.task('build:html', () => {
     return gulp.src([
         `${devDir}/**/*.html`
-    ],{
+    ], {
         
-    }).pipe(gulp.dest(`${appDir}/static/html/`));
+        }).pipe(gulp.dest(`${appDir}/static/html/`));
 })
 
-gulp.task('build:scss',()=>{
-    
-    var self = this;
-    
+gulp.task('build:scss', () => {
     return gulp.src([
         `${devDir}/**/*.scss`
-        ])
-        .pipe($.plumber((error)=>{
-            gutil.beep;
-            gutil.log(gutil.colors.red('Error (' + error.plugin + '): ' + error.message));
-            this.emit('end');
+    ])
+        .pipe($.plumber({
+            errorHandler: function (error) {
+                gutil.log(gutil.colors.red('Error (' + error.plugin + '): ' + error.message));
+                this.emit('end');
+            }
         }))
-        .pipe($.sass({outputStyle:'expanded'}))
-        // .on('error', gutil.log)
-        // .on('error', gutil.beep)
+        .pipe($.sass({ outputStyle: 'expanded' }))
         .pipe($.autoprefixer({
             browsers: ['last 10 versions'],
             cascade: false
@@ -43,12 +39,11 @@ gulp.task('build:scss',()=>{
         .pipe(gulp.dest(`${appDir}/static/css/`))
 })
 
-
-gulp.task('watch',()=>{
+gulp.task('watch',['build:html','build:scss'], () => {
     gulp.watch(`${devDir}/**/*.html`, ['build:html']);
     gulp.watch(`${devDir}/**/*.scss`, ['build:scss'])
 })
 
-gulp.task('default',()=>{
+gulp.task('default', () => {
     console.log('default task')
 })
