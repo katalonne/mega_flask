a1520695b7857ff74dd23658339bfa8facd2b1de
Catalin <catalin4ic@gmail.com>
2016-08-04 19:49:25 +0300

    no print

--- app/views_modules/main_views.py
+++ app/views_modules/main_views.py
@@ -8,14 +8,14 @@ main = Blueprint('main',__name__)
 @main.route('/')
 @main.route('/index')
 def index():
-    print('request geaders ; ' + request.headers.get('User-Agent'))
+    # print('request geaders ; ' + request.headers.get('User-Agent'))
     user_agent = request.headers.get('User-Agent')
     iPhone = bool(re.search(r"iPhone", user_agent))
     Android = bool(re.search(r"Android", user_agent))
     iPad = bool(re.search(r"iPad", user_agent))
     Windows_Phone = bool(re.search(r"Windows Phone", user_agent))
     mobile = iPhone or Android or iPad or Windows_Phone
-    print(mobile)
+    # print(mobile)
     random_nr = random.randint(1,12)
     user = {'nickname': 'Katalonne'}  # fake user
     posts = [  # fake array of posts
